cf8237ffbb0f337112f827da54eb2bbf8ce5f031
anyaachan <anna.danchenko@icloud.com>
2024-03-28 23:22:27 +0300

    add stroke smoothing

--- static/js/script.js
+++ static/js/script.js
@@ -22,7 +22,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
         // Get ratio of the canvas buffer size to the element size. 
         // Buffer Size - actual drawing area in pixels
-        // Element size - physical size of the element as specified in the css -> displayed on the screem
+        // Element size - physical size of the element as specified in the css -> displayed on the screen
         const scaleX = canvas.width / rect.width; 
         const scaleY = canvas.height / rect.height; 
 
@@ -34,6 +34,12 @@ document.addEventListener('DOMContentLoaded', (event) => {
         }
     }
 
+    // Set the stroke style
+    ctx.lineWidth = 8;
+    ctx.lineJoin = 'round';
+    ctx.lineCap = 'round';
+    ctx.strokeStyle = '#B70000';
+
     // Initiate drawing when mouse is pressed
     canvas.onmousedown = (e) => {
         drawing = true;
@@ -43,35 +49,46 @@ document.addEventListener('DOMContentLoaded', (event) => {
         
         // Set drawing cursor to the start position
         path.moveTo(x, y);
+        path.lineTo(x, y);
+        ctx.stroke(path);
+        lastX = x;
+        lastY = y;
     }
 
-    const lastX = 0;
-    const lastY = 0;
-    
     // Draw on canvas as the mouse is moved
     canvas.onmousemove = (e) => {
         // If the mouse is pressed, draw
         if (drawing) {
             const {x, y} = getEventCanvasPosition(e);
 
-            // Set the stroke style for the path
-            ctx.lineWidth = 8;
-            ctx.lineJoin = 'round';
-            ctx.lineCap = 'round';
-            // Draw a line by connecting the last point in the current path to the x, y coordinates
-            path.quadraticCurveTo(x, y, x, y);
+            // Calculate the midpoint between the last point and the current point
+            let midX = (lastX + x) / 2;
+            let midY = (lastY + y) / 2;
+
+            path.quadraticCurveTo(lastX, lastY, midX, midY);
 
             // Visually render the path on the canvas with the specified stroke style
             ctx.stroke(path);
+
+            lastX = x;
+            lastY = y;
         }
     }
 
-    // Stop drawing when mouse is released or cursor leaves canvas area
+    // Stop drawing when mouse is released
     canvas.onmouseup = () => {
         drawing = false;
     }
 
-    canvas.onmouseleave = () => {
-        drawing = false;
+    canvas.onmouseleave = (e) => {
+        if(drawing) {
+            drawing = false;
+
+            const {x, y} = getEventCanvasPosition(e);
+            path.lineTo(x, y);
+            ctx.stroke(path);
+        }
     }
+
+
 });
