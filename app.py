fc8e3280806ed4e702ec7c26ff1a71d41d085d40
anyaachan <anna.danchenko@icloud.com>
2024-04-04 18:17:49 +0300

    add necessary modules

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 from flask import Flask, render_template, request, jsonify
+import base64
+from PIL import Image
+import io
 
 app = Flask(__name__)
 
@@ -25,9 +28,17 @@ def pause():
 @app.route("/upload-image", methods = ['POST'])
 def upload_image():
     data = request.json["image"] # Convert json to dictionary. This line extracts the Base64-encoded image data from the request payload using the key 'image'.
+    # Decode the data
+    header, encoded = data.split(",", 1)
+    bytes_data = base64.b64decode(encoded)
+
+    #Convert to an image 
+    image = Image.open(io.BytesIO(bytes_data))
 
     # Call model evaluation function here
-    return jsonify(result="The result from your model")
+
+    # Return response as a JSON 
+    return jsonify(result=data)
 
 if __name__ == '__main__':
     app.run(debug=True)
